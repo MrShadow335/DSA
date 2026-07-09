148105506a3dfce18135929a3aa388aee393545a
MrShadow335 <letmeidentifymyself@gmail.com>
2026-07-10 00:17:35 +0530

    Added

---
+++ EXAM/que1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class que1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt;
+        int 
+        for(int i = 2;i<n;i++){
+            if(n)
+        }
+    }
+}
