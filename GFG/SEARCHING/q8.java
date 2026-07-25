9fe5512ff3cad85650d27021b7dc923ba497107c
MrShadow335 <letmeidentifymyself@gmail.com>
2026-07-25 22:00:05 +0530

    Updated

--- GFG/SEARCHING/q8.java
+++ GFG/SEARCHING/q8.java
@@ -2,20 +2,26 @@ public class q8{
     public static void sqRoot(int n){
         int low = 1;
         int high = n;
+        int ans = 0;
         if(n==0) System.out.println(0);
         while(low<=high){
             int mid = low + (high - low) / 2;
             if(mid*mid == n) {
-                System.out.println(mid);
+                ans = mid;
+                
                 break;
             }
             else if(mid*mid > n) high = mid -1;
-            else low = mid + 1;
+            else{
+                low = mid + 1;
+                ans = mid;
+            } 
         }
+        System.out.println(ans);
         
     }
     public static void main(String[] args){
-        int n = 25;
+        int n = 66;
         //BRUTE
         // int root = 0;
         // for(int i = 0;i<n;i++){
