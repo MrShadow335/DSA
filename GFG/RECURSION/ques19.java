7ee154aa41b9659e9908c225be36dd13a3fe3e27
MrShadow335 <letmeidentifymyself@gmail.com>
2026-07-27 14:00:09 +0530

    Updated

--- GFG/RECURSION/ques19.java
+++ GFG/RECURSION/ques19.java
@@ -3,10 +3,10 @@
  */
 import java.util.*;
 public class ques19 {
-    public static void generate(int n, int l, int r, String s, List<String> ans) {
+    public static int generate(int n, int l, int r, String s, List<String> ans) {
         if(s.length()==2*n){
             ans.add(s);
-            return;
+            return ans;
         }
         if(l<n) generate(n, l+1, r, s+"(", ans);
         if(r<l) generate(n, l, r+1, s+")", ans);
@@ -14,9 +14,9 @@ public class ques19 {
     public static void main(String[] args) {
         List<String> ans = new ArrayList<>();
         int n =3;
-        generate(n, 0,0,"", ans);
+        int sol = generate(n, 0,0,"", ans);
         // return ans;
-        System.out.println(ans);
+        System.out.println(sol);
     }
     
 }
