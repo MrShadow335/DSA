268a24c13dd94a2706efe6493adaf3c29fc4ac09
MrShadow335 <letmeidentifymyself@gmail.com>
2026-07-27 10:43:46 +0530

    Updated 3 Java file(s)

--- GFG/RECURSION/ques7.java
+++ GFG/RECURSION/ques7.java
@@ -2,7 +2,7 @@ public class ques7{
     public static void main(String[] args){
         int n = 1230450;
         int rev = 0;
-        reverse(n,rev);
+        reverse(rev, n);
         // while(n>0){
         //     rev = rev*10;
         //     rev = rev + n%10;
@@ -10,11 +10,12 @@ public class ques7{
         // }
         // System.out.println(rev);
     }
-    public static void reverse(int n, int rev){
+    public static void reverse(int rev, int n){
         if(n==0){
             System.out.print(rev);
             return;
         }
-        reverse(n/10, rev*10 + n%10);
+        // reverse(n/10, rev*10 + n%10);
+        reverse( rev*10 + n%10, n/10);
     }
 }
