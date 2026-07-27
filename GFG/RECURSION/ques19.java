e4e08b7ac816a91e611c1ebcd65a76235c9cee77
MrShadow335 <letmeidentifymyself@gmail.com>
2026-07-27 22:00:04 +0530

    Updated

--- GFG/RECURSION/ques19.java
+++ GFG/RECURSION/ques19.java
@@ -3,19 +3,38 @@
  */
 import java.util.*;
 public class ques19 {
-    public static int generate(int n, int l, int r, String s, List<String> ans) {
-        if(s.length()==2*n){
-            ans.add(s);
-            return ans;
-        }
-        if(l<n) generate(n, l+1, r, s+"(", ans);
-        if(r<l) generate(n, l, r+1, s+")", ans);
+    // public static void generate(int n, int l, int r, String s, List<String> ans) {
+    //     if(s.length()==2*n){
+    //         ans.add(s);
+    //         return ;
+    //     }
+    //     if(l<n) generate(n, l+1, r, s+"(", ans);
+    //     if(r<l) generate(n, l, r+1, s+")", ans);
+        
+    // }
+    // public static void main(String[] args) {
+    //     List<String> ans = new ArrayList<>();
+    //     int n =3;
+    //     generate(n, 0,0,"", ans);
+    //     // return ans;
+    //     System.out.println(ans);
+    // }
+
+
+    
+    public static List<String> generate(int n, int l, int r, String s, List<String> ans) {
+    if (s.length() == 2 *n) {
+        ans.add(s);
+        return ans;
     }
+    if (l < n) generate(n, l + 1, r, s + "(", ans);
+    if (r < l) generate(n, l, r + 1, s + ")", ans);
+    return ans;
+}
     public static void main(String[] args) {
         List<String> ans = new ArrayList<>();
-        int n =3;
-        int sol = generate(n, 0,0,"", ans);
-        // return ans;
+       
+        List<String> sol = generate(3, 0, 0, "", ans);
         System.out.println(sol);
     }
     
