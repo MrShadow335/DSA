fcfdfef12f5bc296b769fe94d8bdc6da7d94b8c4
MrShadow335 <letmeidentifymyself@gmail.com>
2026-07-30 22:41:50 +0530

    Updated 2 Java file(s)

--- GFG/OOPS/q1.java
+++ GFG/OOPS/q1.java
@@ -1,5 +1,24 @@
-package GFG.OOPS;
 
+import java.util.*;
 public class q1 {
-    
+    public static class Student{
+        int rNo;
+        String name;
+        double cgpa;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Student s1 = new Student();
+        s1.name = "ABhi";
+        System.out.println("Enter your Roll No ->");
+        s1.rNo = sc.nextInt();
+        s1.cgpa = 8.01;
+        System.out.println(s1.name+" "+s1.rNo+" "+s1.cgpa);
+
+        Student s2 = new Student();
+        s2.name = "Nishi";
+        s2.rNo = 13;
+        s2.cgpa = 9.37;
+        System.out.println(s2.name+" "+s2.rNo+" "+s2.cgpa);
+    }
 }
