07f195309a7f7291e135cb51e3edc1694bf3def4
Abhishek Kumar <letmeidentifymyself@gmail.com>
2026-08-04 22:32:25 +0530

    Updated 5 Java file(s)

---
+++ GFG/LINKEDLIST/q2.java
@@ -0,0 +1,13 @@
+public class q2 {
+    static class Node{
+        int val;
+        Node next;
+        Node(int val){
+            this.val = val;
+        }
+    }
+    public static void main(String[] args) {
+        Node a = new Node(10);
+    }
+    
+}
